3d0fab035fae39813d6cb460e449230f5bd870d6
Nicat Akbarli <nicatakbarli571@gmail.com>
2023-10-28 00:56:02 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+
+
+
+
+string[] values = "salam salam salam salam salam salanm salsma ala ala ala ala ala ala ala ala ala ala ala".Split(' ');
+var counts = new Dictionary<string, int>();
+
+foreach (string value in values) { 
+ 
+ if (counts.ContainsKey(value)){
+         counts[value] = counts[value] + 1;
+         var b = counts[value] + value;
+     var  sde = counts[value];
+    // System.Console.WriteLine(sde + ":"+value);
+    }
+    else {
+     
+        counts.Add(value, 1);
+        var a =counts[value] + value;
+        
+       
+         var  id = counts[value];
+        
+ // System.Console.WriteLine(id+value);
+    }
+}
+
+foreach(var nicat in counts ){
+    System.Console.WriteLine(nicat);
+}
+
+
+
+
+
+
+
+
+
+
+
